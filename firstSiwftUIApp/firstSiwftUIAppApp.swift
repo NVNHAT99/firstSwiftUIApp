321d5978db3e944fc4344f1ea4298f17b9e67812
NhatNV99 <cauutnhala@gmail.com>
2021-08-21 11:27:54 +0700

    first commit

---
+++ firstSiwftUIApp/firstSiwftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSiwftUIAppApp.swift
+//  firstSiwftUIApp
+//
+//  Created by NhatNguyen on 8/21/21.
+//
+
+import SwiftUI
+
+@main
+struct firstSiwftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
